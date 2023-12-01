d376955364017f507e8c4b7a5d9b557added0045
Igor M. <samplefines@gmail.com>
2023-12-01 16:03:13 +0300

    moveballs auto on mobile; on PC -onmousenter

--- src/scripts/moveBalls.js
+++ src/scripts/moveBalls.js
@@ -18,6 +18,7 @@ function moveOnce(arr, x, y) {
             el.style.transform = 'translate(' + (Math.random() * (2 * x) - x) + 'rem, ' + (Math.random() * (2 * y) - y) + 'rem) ' + 'scale(' + (0.4 + Math.random() * 1.1) + ')';
         }
     }
+    console.log('moveOnce(arr, x, y)')
 }
 
 // задаём переменную в которую будем сохранять "идентификатор для регулярного запуска функции" moveBalls - идентификатор возвращает функция setInterval() -, чтобы потом можно было остановить её повторяющееся выполнение через clearInterval(). При вызове clearInterval() браузер найдёт таймер (из список активных таймеров браузера) по идентификатору и уберёт его из списка - повторение функции moveBalls прекратится.
@@ -29,7 +30,10 @@ function moveBalls(section, arr, x, y) {
     mbId = setInterval(moveOnce, 4000, arr, x, y);
 
     // вешаем листнер на остановку анимации. Благодаря опции  { once: true }  - обработчик сработает один раз, а затем автоматически удалится. ({ once: true } можно даже использовать с анонимными функциями и не нужно будет очищать обработчик  - он удалится автоматически.)
-    section.addEventListener('mouseleave', () => { stopBalls(section, arr) }, { once: true });
+    //  вешаем листнер только если устройства "мышь".
+    if (window.matchMedia('(hover: hover)').matches || window.matchMedia('(pointer: fine)').matches) {
+        section.addEventListener('mouseleave', () => { stopBalls(section, arr) }, { once: true });
+    }
 }
 
 // Создаём функцию, которая остановит регулярный перезапуск moveOnce. - Функции clearInterval() передаём аргументом идентификатор регулярного перезапуска функции - идентификатор, который возвращает функция setInterval() - мы его сохранили в переменную mbId
@@ -67,10 +71,14 @@ function stopBalls(section, arr) {
     }
 
     // вешаем листнер на старт анимации. Благодаря опции  { once: true }  - обработчик сработает один раз, а затем автоматически удалится.
-    section.addEventListener('mouseenter', () => { moveBalls(section, arr, 80, 40) }, { once: true });
+    //  вешаем листнер только если устройства ввода "мышь".
+    if (window.matchMedia('(hover: hover)').matches || window.matchMedia('(pointer: fine)').matches) {
+        section.addEventListener('mouseenter', () => { moveBalls(section, arr, 80, 40) }, { once: true });
+    }
 }
 
 // // вешаем первоначальный (после загрузки страницы) листнер на старт анимации. Благодаря опции  { once: true }  - обработчик сработает один раз, а затем автоматически удалится. (убрать листнер  с функцией с аргументами  через removeEventListener - не получается, внутрь безымянной функции-обёртки класть не стал)
+// вешаем после проверки что устройство поддерживает устройство ввода "мышь" - строка ~250
 // video.addEventListener('mouseenter', () => { moveBalls(video, vballs, 80, 40) }, { once: true });
 
 // не экономя память - просто вешаем листнеры
@@ -83,13 +91,12 @@ function stopBalls(section, arr) {
 const video2 = document.getElementById('video2');
 const vballsYtb = document.querySelectorAll('.video__ball_ytb');
 
+// вешаем после проверки что устройство поддерживает устройство ввода "мышь" - строка ~250
 // video2.addEventListener('mouseenter', () => { moveBalls(video2, vballsYtb, 80, 40) }, { once: true })
 
 
 
-
-
-// остановливаем анимацию движения шаров после нажатия  на кнопку воспроизведения видео
+// останавливаем анимацию движения шаров после нажатия  на кнопку воспроизведения видео
 const btn1 = document.querySelector('.video__btn');
 const btn2 = document.querySelector('.video__btn_2');
 
@@ -147,6 +154,7 @@ btn.onclick = () => {
 
 let mbIdAftMod;
 
+
 modal.onclick = (event) => {
     if (event.target === modal) {
         videoJS.pause();  // - !! из-за этого не работает закрытие видео с youtube (во frame-е).
@@ -184,6 +192,7 @@ modal.onclick = (event) => {
                     el.style.transform = 'translate(' + (Math.random() * (2 * x) - x) + 'rem, ' + (Math.random() * (2 * y) - y) + 'rem) ' + 'scale(' + (0.4 + Math.random() * 1.1) + ')';
                 }
             }
+            console.log('moveOnceAftMod')
         }
 
         function moveBallsAftMod(section, arr, x, y) {
@@ -191,7 +200,11 @@ modal.onclick = (event) => {
                 moveOnceAftMod(arr, x, y);
             }, 500);
             mbIdAftMod = setInterval(moveOnceAftMod, 4000, arr, x, y);
-            section.addEventListener('mouseleave', () => { stopBallsAftMod(section, arr) }, { once: true });
+
+            // вешаем после проверки что устройство поддерживает устройство ввода "мышь"
+            if (window.matchMedia('(hover: hover)').matches || window.matchMedia('(pointer: fine)').matches) {
+                section.addEventListener('mouseleave', () => { stopBallsAftMod(section, arr) }, { once: true });
+            }
         }
 
         function stopBallsAftMod(section, arr) {
@@ -200,12 +213,14 @@ modal.onclick = (event) => {
 
         }
 
+
         moveBallsAftMod(videoLocal, vballs, 80, 40);
 
 
     }
 
 }
+
 
 
 // стартуем движение шаров при попадании в область видимости на мобильных
@@ -240,6 +255,7 @@ modal.onclick = (event) => {
 //     // }
 // }
 
+// проверяем что устройство не поддерживает устройство ввода "мышь" а тачи и т.д.
 if (window.matchMedia('(hover: none)').matches || window.matchMedia('(pointer: coarse)').matches) {
 
     video.currentBalls = vballs;
@@ -256,53 +272,24 @@ if (window.matchMedia('(hover: none)').matches || window.matchMedia('(pointer: c
                     // stopBalls(entry.target, entry.target.currentBalls)
                     // console.log(entry.target);
                     // console.log('stopBalls()');
+
                     // чтобы успело примениться transition к шарикам (entry.target.currentBalls)
                     setTimeout(() => {
-                        moveOnce(entry.target.currentBalls, 80, 40);
-                        mbId = setInterval(moveOnce, 4000, entry.target.currentBalls, 80, 40);
+                        moveBalls(section = null, entry.target.currentBalls, 80, 40)
 
                         console.log(entry.target);
                         console.log('start moveBalls()');
                     }, 33);
 
                 } else {
+                    stopBalls(section = null, entry.target.currentBalls);
 
-                    // stopBalls(entry.target, entry.target.currentBalls)
-
-                    // прервали повторение выполнения функции перемещения элементов moveOnce
-                    clearInterval(mbId);
-                    console.log("stopBalls runs and clearInterval(mbId)")
-
-                    for (let el of entry.target.currentBalls) {
-                        //  зададём продолжительность transition 0.5s для  элементов - чтобы плавно но быстро вернуть их в начальное место.
-                        el.style.transition = 'all 0.5s linear';
-
-                        //  для каждой ширины экрана возвращаем начальные свойства transform  для элементов
-                        if (window.innerWidth < 425) {
-                            el.style.transform = 'translate(0, 0) scale(0.35)';
-                        } else {
-                            if (window.innerWidth < 768) {
-                                el.style.transform = 'translate(0, 0) scale(0.5)';
-                            } else {
-                                if (window.innerWidth < 1024) {
-                                    el.style.transform = 'translate(0, 0) scale(0.75)';
-                                } else {
-
-                                    el.style.transform = 'translate(0, 0)';
-                                }
-                            }
-                        }
-                        // после выполнения остального js кода - возвращаем изначальное значение transition = 12s для всех элементов - чтоб при следующих запусках перемещений элементы двигались с изначально заданной скоростью.
-                        //  Хотя transition =  12s присваивается элементам ещё в момент анимации с transition = 0.5s  - анимация всё равно продолжается время 0.5s - как это было указано во время начала анимации
-                        setTimeout(
-                            () => {
-                                el.style.transition = 'all 12s linear';
-                            }
-                            , 16)
-                    }
+                    clearInterval(mbIdAftMod); //останавливаем движение запущенное закрытием модального окна (если оно было открыто)
 
                     console.log(entry.target);
                     console.log('stopBalls()');
+                    console.log('clearInterval(mbIdAftMod)');
+
                 }
             }
             )
